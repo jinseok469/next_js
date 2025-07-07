ca7c1ae3b329deed03aa9ef65c2cc00875186feb
jinseok469 <jinseok469@naver.com>
2025-07-07 17:21:02 +0900

    0707 next js 공부

--- onbite-next/section02/src/pages/_app.tsx
+++ onbite-next/section02/src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickButton =  () =>{
+    router.push("/test");
+  }
+  return <div>
+    <header>
+      <Link href={"/"}>index</Link>
+      <br></br>
+      <Link href={"/search"}>search</Link>
+      <br></br>
+      <Link href={"/book"}>book</Link>
+      <div>
+        <button onClick={()=>onClickButton()}>/test 페이지로 이동</button>
+      </div>
+    </header>
+    
+    <Component {...pageProps} /></div>;
 }
