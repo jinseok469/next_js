98ed130eaddf3c10b0cd81d9483b3930de089d0d
jinseok469 <jinseok469@naver.com>
2025-07-16 17:44:50 +0900

    0716 next js 공부

--- onbite-next/section02/src/pages/_app.tsx
+++ onbite-next/section02/src/pages/_app.tsx
@@ -1,24 +1,12 @@
+import GlobalLayout from "@/components/global-layout";
+import SearchableLayout from "@/components/searchable-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton =  () =>{
-    router.push("/test");
-  }
-  return <div>
-    <header>
-      <Link href={"/"}>index</Link>
-      <br></br>
-      <Link href={"/search"}>search</Link>
-      <br></br>
-      <Link href={"/book"}>book</Link>
-      <div>
-        <button onClick={()=>onClickButton()}>/test 페이지로 이동</button>
-      </div>
-    </header>
-    
-    <Component {...pageProps} /></div>;
+  return <GlobalLayout>
+    <SearchableLayout>
+    <Component {...pageProps}></Component>
+    </SearchableLayout>
+  </GlobalLayout>
 }
